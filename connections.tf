450a7e531de6d6302067e30e9d8f493ba382b6ec
Kseniia Ryuma <kryuma@Kseniias-MacBook-Pro.local>
2019-11-14 11:24:56 -0800

    vpn

---
+++ connections.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+provider "google" {
+  credentials = "${file("/Users/kryuma/Desktop/terraform_oci/igneous-axiom-259004-74efcf7ef80f.json")}"
+  project     = "igneous-axiom-259004"
+  region      = "us-central1"
+}
+
+//provider "azurerm" {
+//  subscription_id = "o"
+//  client_id = "1"
+//  client_secret = "2"
+//  tenant_id = "3"
+//}
